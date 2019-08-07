d3330373443c2c443c40651fcffbc008febb4eef
Kyle Morrell <kyle@morrellwolrd.net>
2019-08-07 15:56:34 -0400

    IsGrouned method

--- Mario/Assets/Movement.cs
+++ Mario/Assets/Movement.cs
@@ -10,6 +10,8 @@ public class Movement : MonoBehaviour
     public SpriteRenderer sr;
     public Rigidbody rb;
     private Animator anim;
+    //public bool grounded;
+    public LayerMask floor;
 
     // Start is called before the first frame update
     void Start()
@@ -17,11 +19,13 @@ public class Movement : MonoBehaviour
         sr = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+        print(isGrounded());
         speed = Input.GetAxis("Horizontal") * 15f;
         transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
         if (Input.GetKey(KeyCode.D))
@@ -46,5 +50,15 @@ public class Movement : MonoBehaviour
                 sr.sprite = rightSprite;
             }
         }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 14), ForceMode2D.Impulse);
+        }
     }
+
+    bool isGrounded()
+    {
+        return GetComponent<BoxCollider2D>().IsTouchingLayers(floor);
+    }
+
 }
